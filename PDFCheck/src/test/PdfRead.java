1f1edafd1a85ca5be5b3baf7eb34c5e5a23a717f
Shreay <34499960+Shreay@users.noreply.github.com>
2018-05-28 22:34:17 +0530

    Update PdfRead.java

--- PDFCheck/src/test/PdfRead.java
+++ PDFCheck/src/test/PdfRead.java
@@ -5,6 +5,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
@@ -16,38 +22,50 @@ import org.xml.sax.SAXException;
 
 public class PdfRead {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 	    InputStream is = null;
 	    final String folder_path = "Files/";
+	    final File folder = new File(folder_path);
+	    AutoDetectParser parser = new AutoDetectParser();
+        ContentHandler handler = new BodyContentHandler(-1);
+        Metadata metadata = new Metadata();
+        Map<String, Integer> map=new HashMap<String, Integer>();
         try {
-            is = new BufferedInputStream(new FileInputStream(new File(folder_path+"online_collaboration.pdf")));
-
-            AutoDetectParser parser = new AutoDetectParser();
-            ContentHandler handler = new BodyContentHandler(-1);
-            Metadata metadata = new Metadata();
-            int i =0;
-            parser.parse(is, handler, metadata);
-            for(String line : handler.toString().split("\n")){
-            	if(line != null){
-            		// System.out.println("Line is  " + line);
-            		for(String word : line.split(" ")){
-            			if(word != null && word.toLowerCase().indexOf("client")>-1){
-        					System.out.println(word);
-        					i++;
-            			}
-            		}
-            		
-            	}
-            }
-            System.out.println("total word hits: "+String.valueOf(i));
-            /*for (String name : metadata.names()) {
-                String value = metadata.get(name);
-
-                if (value != null) {
-                    System.out.println("Metadata Name:  " + name);
-                    System.out.println("Metadata Value: " + value);
+        	System.out.println("Start");
+        	//read each file in folder one by one
+        	for(File pdfFile : folder.listFiles()){
+        		is = new BufferedInputStream(new FileInputStream(pdfFile));
+        		int i =0;   //word count
+        		parser.parse(is, handler, metadata);   //reading the pdf file
+        		//Split by each line
+        		for(String line : handler.toString().split("\n")){
+                	if(line != null){
+                		//Split by each word
+                		for(String word : line.split(" ")){
+                			if(word != null && word.toLowerCase().indexOf("client")>-1){
+            					//System.out.println(word);
+            					i++;
+                			}
+                		}
+                		
+                	}
                 }
-            }*/
+        		
+        		map.put(pdfFile.getName(),i);
+        	}
+
+            
+        	List<Object> list=new ArrayList(map.entrySet());
+        	
+        	//Soring in reverse order
+        	Collections.sort(list,new Comparator(){
+        	public int compare(Object obj1, Object obj2){
+        	return ((Comparable)((Map.Entry)(obj2)).getValue()).compareTo(((Map.Entry)(obj1)).getValue());
+        	}
+        	});
+        	System.out.println("File list sorted based on occurances(File Name: num of occurances) : "+list);
+            
             System.out.println("Done");
         } catch (IOException e) {
             e.printStackTrace();
